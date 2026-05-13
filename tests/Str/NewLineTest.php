761400e05c781ad4638c91bc590ddd32485249ce
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2026-05-13 10:57:14 +0200

    Fix failing test

--- tests/Str/NewLineTest.php
+++ tests/Str/NewLineTest.php
@@ -3,5 +3,5 @@
 use function Spatie\Piper\Str\newLine;
 
 it('appends newline characters', function () {
-    expect('Laravel' |> newLine(2))->toBe("Laravel\n\n");
+    expect('Laravel' |> newLine(2))->toBe('Laravel'.PHP_EOL.PHP_EOL);
 });
